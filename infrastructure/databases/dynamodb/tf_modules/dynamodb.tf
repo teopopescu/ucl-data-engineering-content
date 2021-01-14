53d7a2193baa53f2f59418fd6f517ec835432e7b
Bernard Louis Alecu <louis.alecu@cognitivecredit.com>
2021-01-14 16:43:09 +0000

    Progress

--- infrastructure/databases/dynamodb/tf_modules/dynamodb.tf
+++ infrastructure/databases/dynamodb/tf_modules/dynamodb.tf
@@ -1,8 +1,8 @@
 resource "aws_dynamodb_table" "basic-dynamodb-table" {
   name           = "GameScores"
   billing_mode   = "PROVISIONED"
-  read_capacity  = 20
-  write_capacity = 20
+  read_capacity  = 5
+  write_capacity = 5
   hash_key       = "UserId"
   range_key      = "GameTitle"
 
@@ -21,17 +21,12 @@ resource "aws_dynamodb_table" "basic-dynamodb-table" {
     type = "N"
   }
 
-  ttl {
-    attribute_name = "TimeToExist"
-    enabled        = false
-  }
-
   global_secondary_index {
     name               = "GameTitleIndex"
     hash_key           = "GameTitle"
     range_key          = "TopScore"
-    write_capacity     = 10
-    read_capacity      = 10
+    write_capacity     = 5
+    read_capacity      = 5
     projection_type    = "INCLUDE"
     non_key_attributes = ["UserId"]
   }
